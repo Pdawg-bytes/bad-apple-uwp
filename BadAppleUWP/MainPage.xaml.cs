4d5a73044ad7b4a3bdd7b5c7ddb7b46778b3d5ac
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2022-10-25 19:34:46 -0400

    Add comments to code

--- BadAppleUWP/MainPage.xaml.cs
+++ BadAppleUWP/MainPage.xaml.cs
@@ -36,6 +36,8 @@ namespace BadAppleUWP
         public MainPage()
         {
             this.InitializeComponent();
+
+            // Defines song player URI and sets variables
             Uri songUri = new Uri("ms-appx:///Assets/Sounds/bad_apple.mp3");
             songPlayer.Source = songUri;
             SongVolume.Value = 100;
@@ -46,10 +48,12 @@ namespace BadAppleUWP
             Timer.Interval = TimeSpan.FromMilliseconds(44);
         }
 
+        // Double to be used for volume control
         public static double SongVolDouble;
 
         private void play_Click(object sender, RoutedEventArgs e)
         {
+            // Pretty self explanitory
             songPlayer.Play();
             Timer.Start();
             play.IsEnabled = false;
@@ -57,6 +61,7 @@ namespace BadAppleUWP
 
         private void pause_Click(object sender, RoutedEventArgs e)
         {
+            // Pretty self explanitory
             songPlayer.Pause();
             Timer.Stop();
             play.IsEnabled = true;
@@ -64,22 +69,30 @@ namespace BadAppleUWP
 
         private void SongVolume_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
+            // Pretty self explanitory
             SongVolDouble = e.NewValue / 100;
             songPlayer.Volume = Math.Round(SongVolDouble, 2);
         }
 
+        // Creates image variable
         public static BitmapImage FrameImage;
+        // Defines frame count
         public static int framei;
         private void Timer_Tick(object sender, object e)
         {
+            // Adds 1 to move onto the next frame
             framei++;
+            // Sets frame source
             FrameImage = new BitmapImage(new Uri("ms-appx:///Assets/Frames/" + framei + ".jpg"));
+            // Sets image source
             BadAppleFrame.Source = FrameImage;
+            // Adds to frame counter value
             FrameNumberBox.Value++;
         }
 
         private void songPlayer_MediaEnded(object sender, RoutedEventArgs e)
         {
+            // Pretty self explanitory
             Timer.Stop();
             framei = 0;
             FrameImage = new BitmapImage(new Uri("ms-appx:///Assets/Frames/1.jpg"));
@@ -90,6 +103,7 @@ namespace BadAppleUWP
 
         private void reset_Click(object sender, RoutedEventArgs e)
         {
+            // Pretty self explanitory
             Timer.Stop();
             framei = 0;
             FrameImage = new BitmapImage(new Uri("ms-appx:///Assets/Frames/1.jpg"));
